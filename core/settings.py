61651f2bccc44d7f915bbea66c544fbadc72b608
matadata <bailove1@gmail.com>
2020-09-28 14:42:26 +0200

    update

--- core/settings.py
+++ core/settings.py
@@ -155,23 +155,52 @@ AUTHENTICATION_BACKENDS = [
     'authentication.authentication.EmailOrPhoneModelBackend',  # to be able to login with email, described next
 ]
 
+
+
 LOGGING = {
     'version': 1,
-    'disable_existing_loggers': False,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'standard': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+    },
     'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
+        'null': {
+            'level':'DEBUG',
+            'class':'django.utils.log.NullHandler',
+        },
+        'logfile': {
+            'level':'DEBUG',
+            'class':'logging.handlers.RotatingFileHandler',
             'filename': 'debug.log',
+            'maxBytes': 50000,
+            'backupCount': 2,
+            'formatter': 'standard',
+        },
+        'console':{
+            'level':'INFO',
+            'class':'logging.StreamHandler',
+            'formatter': 'standard'
         },
     },
     'loggers': {
         'django': {
-            'handlers': ['file','console'],
-            'level': 'DEBUG',
+            'handlers':['console'],
             'propagate': True,
+            'level':'WARN',
         },
-    },
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        'MYAPP': {
+            'handlers': ['console', 'logfile'],
+            'level': 'DEBUG',
+        },
+    }
 }
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
