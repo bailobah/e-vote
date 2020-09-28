af6640e2fa93d4deef4cb5f3a6ea74a10785018b
matadata <bailove1@gmail.com>
2020-09-28 14:37:41 +0200

    update

--- core/settings.py
+++ core/settings.py
@@ -167,7 +167,7 @@ LOGGING = {
     },
     'loggers': {
         'django': {
-            'handlers': ['file'],
+            'handlers': ['file','console'],
             'level': 'DEBUG',
             'propagate': True,
         },
