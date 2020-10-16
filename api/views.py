351e0d30fa4f840ce7fb3b6c774708947041b6dd
bailobah <bailove1@gmail.com>
2020-10-16 18:30:07 +0000

    update errormessage

--- api/views.py
+++ api/views.py
@@ -13,7 +13,7 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND, HTTP_200_OK
 from rest_framework.views import APIView
 
-from api.models import GetMinuteSmsSerializer, MinuteSms, MinuteDetailsSms
+from api.models import GetMinuteSmsSerializer, MinuteSms, MinuteDetailsSms, RejetedSms
 from election.models import PollingStation, PollingStationSerializer, MinuteSerializer, Minute, GetMinuteSerializer, \
     Election
 from locality.models import Allocation
