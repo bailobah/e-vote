913280ab5a9727467774595c0af65f063bf62b70
bailobah <bailove1@gmail.com>
2020-10-18 04:33:31 +0000

    add sms back

--- sms/views.py
+++ sms/views.py
@@ -13,7 +13,7 @@ from api.models import RejectedSms
 
 def sms_list(request):
 
-    sms = RejectedSms.objects.all().order_by('-id')
+    sms = RejectedSms.objects.filter(is_active=True).order_by('-id')
 
     page = request.GET.get('page', 1)
     paginator = Paginator(sms, 20)
@@ -26,23 +26,23 @@ def sms_list(request):
 
     return render(request, 'sms/ui-sms.html', {'sms': sms})
 
-# def sms_mode_delete(request, pk):
-#     sms = get_object_or_404(RejectedSms, pk=pk)
-#     print(sms)
-#     data = dict()
-#     if request.method == 'POST':
-#         sms.is_active = False
-#         sms.save()
-#         data['form_is_valid'] = True
-#         sms = RejectedSms.objects.all().order_by('-id')
-#         data['html_sms_list'] = render_to_string('sms/list.html', {
-#             'sms': sms
-#         })
-#     else:
-#         context = {'sms': sms}
-#         data['html_form'] = render_to_string('sms/delete.html',
-#             context,
-#             request=request,
-#         )
-#     return JsonResponse(data)
+def sms_mode_delete(request, pk):
+    sms = get_object_or_404(RejectedSms, pk=pk)
+    print(sms)
+    data = dict()
+    if request.method == 'POST':
+        sms.is_active = False
+        sms.save()
+        data['form_is_valid'] = True
+        sms = RejectedSms.objects.all().order_by('-id')
+        data['html_sms_list'] = render_to_string('sms/list.html', {
+            'sms': sms
+        })
+    else:
+        context = {'sms': sms}
+        data['html_form'] = render_to_string('sms/delete.html',
+            context,
+            request=request,
+        )
+    return JsonResponse(data)
 
